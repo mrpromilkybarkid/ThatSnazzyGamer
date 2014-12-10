dc44451e6e8953e6c1d7f725a052c2f927cf0f77
Ben Powley <ben.powley97@gmail.com>
2014-12-10 13:01:38 +0000

    Contact form on work page

---
+++ index.php
@@ -0,0 +1,54 @@
+<!doctype>
+<html lang="en">
+    <head>
+        <title>ThatSnazzyGamer</title>
+        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" type="text/css" />
+        <link rel="stylesheet" href="//code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css" type="text/css">
+        <link rel="stylesheet" href="css/material.css" type="text/css" />
+        <link rel="stylesheet" href="css/ripples.css" type="text/css" />
+    </head>
+    <style>
+    </style>
+    <body>
+
+        <div class="jumbotron" style="margin-top: 0px; background-color: #3399FF;">
+            <div class="container">
+                <h1 style="text-align: center; text-shadow: 0px 1px 10px #111;"><a id="loadHome" style="text-decoration: none; color: White;" href="index.php">ThatSnazzyGamer</a></h1>
+                <br />
+                <div class="row">
+                    <div class="col-md-offset-5">
+                        <ul class="nav nav-tabs" style="background-color: #3399FF; border: none;">
+                            <li><a id="loadHomeContent" href="#home">Home</a></li>
+                            <li><a id="loadInfoContent" href="#info">Info</a></li>
+                            <li><a id="loadWorkContent" href="#work">Work</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <div class="container" id="contentContainer">
+            <div class="well" id="mainContent">
+            </div>
+        </div>
+        
+        <div class="container" id="buttonContainer">
+            <a title="Previous" id="prev" href="#prev" class="btn btn-info btn-fab btn-raised mdi-navigation-arrow-back"></a>
+            <a title="Next" id="next" style="float: right;" href="#next" class="btn btn-info btn-fab btn-raised mdi-navigation-arrow-forward"></a>
+        </div>
+        
+        <div class="jumbotron" style="margin-top: 0px; background-color: rgba(0, 0, 0, 0); box-shadow: 0 0 0 0;">
+            <div class="container">
+                <h6 style="text-align: center;"><a style="text-decoration: none; color: #bbb;" href="#home"> - ThatSnazzyGamer - </a></h6>
+            </div>
+        </div>
+        
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
+        <script src="js/jquery-ui.js"></script>
+        <script src="js/material.min.js"></script>
+        <script src="js/ripples.min.js"></script>
+        <script src="js/main.js"></script>
+        
+    </body>
+</html>
--- workContent.php
+++ workContent.php
@@ -2,7 +2,41 @@
 <hr />
 <h3 id="mainContent">
     Have an idea for a project? <br />
-    <br />
-    <a title="Email: ben.powley97@gmail.com">Contact Me</a>
 </h3>
-<br />
+
+<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">
+  Contact Me
+</button>
+
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+        <h4 class="modal-title" id="myModalLabel">Contact Me</h4>
+      </div>
+      <div class="modal-body">
+        <form class="form-horizontal">
+            <fieldset>
+                <div class="form-group">
+                    <label for="inputEmail" class="col-lg-2 control-label">Email</label>
+                    <div class="col-lg-10">
+                        <input type="email" class="form-control" id="inputEmail" placeholder="Email...">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="inputMessage" class="col-lg-2 control-label">Message</label>
+                    <div class="col-lg-10">
+                        <textarea class="form-control" id="inputMessafe" placeholder="Message..."></textarea>
+                    </div>
+                </div>
+            </fieldset>  
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
